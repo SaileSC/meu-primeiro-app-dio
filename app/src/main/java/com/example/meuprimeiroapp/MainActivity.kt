d3dda5697c935c712c2537e3b949a8ed4cfe6f1e
SaileSC <sailesantos0@gmail.com>
2023-09-30 12:45:29 -0400

    Requisito do desafio concluido...

---
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.meuprimeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*
+            O desafio sera criar um valor dentro do string.xml e trocar o
+            texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+         */
+    }
+}
